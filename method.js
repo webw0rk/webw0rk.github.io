dc50e4519eafd3d104f09f7917220c90f38f1c86
webw0rk <164522626+webw0rk@users.noreply.github.com>
2025-05-26 16:05:08 +0900

    Update method.js

--- method.js
+++ method.js
@@ -1,3 +1,4 @@
+/* 独自メソッド */
 Window.prototype.$Id = function(id){ return document.getElementById(id); }
 Window.prototype.$Class = function(class_name){
   return Array.from(document.getElementsByClassName(class_name));
@@ -8,3 +9,17 @@ Element.prototype.$Class = function(class_name){
 Array.prototype.each = Array.prototype.forEach;
 NodeList.prototype.each = NodeList.prototype.forEach;
 EventTarget.prototype.on = EventTarget.prototype.addEventListener;
+
+/* コンソールにSelf-XSSの警告を出す */
+(() => {
+  [
+    {
+      txt: '⚠️警告',
+      css: 'color: red; font-size: 28px;'
+    }, {
+      txt: 'ここに何かを貼り付けると Self-XSS により個人情報の盗難やアカウント乗っ取りの被害に遭う恐れがあります。\n今よく分からないものを貼り付けようとしているなら、直ちに中止して下さい！',
+      css: 'font-size: 16px;'
+    }
+  ]
+    .each((txt_data) => { console.log(`%c${txt_data.txt}`, txt_data.css); });
+})();
