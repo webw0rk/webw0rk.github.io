f3951900f26fd14fbc406b0f660ba78985a3b9fd
webw0rk <164522626+webw0rk@users.noreply.github.com>
2025-05-26 16:26:38 +0900

    Add files via upload

---
+++ google-analytics.js
@@ -0,0 +1,15 @@
+/* Googleアナリティクス */
+(() => {
+  const id = 'G-GX5CF824RP';
+
+  const google_analytics_elem = $Id('google-analytics-elem');
+  const script_elem = document.createElement('script');
+  script_elem.async = true;
+  script_elem.src = `https://www.googletagmanager.com/gtag/js?id=${id}`;
+  google_analytics_elem.insertAdjacentElement('afterend', script_elem);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ dataLayer.push(arguments); }
+  gtag('js', new Date());
+  gtag('config', 'G-GX5CF824RP');
+})();
