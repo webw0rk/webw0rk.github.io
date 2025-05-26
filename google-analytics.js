b1049018f1ba8bbf8f58814a2d4cacd7b91eb7e9
webw0rk <164522626+webw0rk@users.noreply.github.com>
2025-05-26 17:25:48 +0900

    Update google-analytics.js

--- google-analytics.js
+++ google-analytics.js
@@ -11,5 +11,5 @@
   window.dataLayer = window.dataLayer || [];
   function gtag(){ dataLayer.push(arguments); }
   gtag('js', new Date());
-  gtag('config', 'G-GX5CF824RP');
+  gtag('config', id);
 })();
